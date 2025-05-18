e21dfdeaa150256a95b4e7ad9d556397df5b218b
Екатерина <diada.var@mail.ru>
2025-05-18 23:28:02 +0300

    1

---
+++ src/main/java/org/skypro/skyshop4/Skyshop4Application.java
@@ -0,0 +1,13 @@
+package org.skypro.skyshop4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skyshop4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skyshop4Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/skyshop4/Skyshop4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.skyshop4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Skyshop4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
